43327bd34dc4b203426c90d585fae8cd0c54a214
Diego Broetto <diego.broetto1@gmail.com>
2025-03-25 16:10:08 -0300

    fix: correct of python code

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 __copyright__ = """
-Copyright 2025 Darede.
+Copyright 2025 Diego Broetto.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
